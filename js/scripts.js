b5993dd2f5164697db03f0efd0421cab1ac07018
Jim Merritt <jim.merritt@intrinzicbrands.com>
2016-05-20 16:31:20 -0400

    Updated config file for gulp

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,47 @@
-window.onload = function () {
+window.onload = function() {
   var pageColor = document.getElementById('keyboard').getAttribute('page-color');
-  var keyPressed = function (event) {
+  var keyPressed = function(event) {
     event.preventDefault();
     var code = event.keyCode;
-    console.log(code);-
     switch (code) {
-    case 16:
-      document.getElementById('shift1').setAttribute('style', 'background-color: ' + pageColor);
-      document.getElementById('shift2').setAttribute('style', 'background-color: ' + pageColor);
-      break;
-    case 17:
-      document.getElementById('control1').setAttribute('style', 'background-color: ' + pageColor);
-      document.getElementById('control2').setAttribute('style', 'background-color: ' + pageColor);
-      break;
-    case 18:
-      document.getElementById('option1').setAttribute('style', 'background-color: ' + pageColor);
-      document.getElementById('option2').setAttribute('style', 'background-color: ' + pageColor);
-      break;
-    default:
-      console.log(code);
-      document.getElementById(code).setAttribute('style', 'background-color: ' + pageColor);
-      break;
+      case 16:
+        document.getElementById('shift1').setAttribute('style', 'background-color: ' + pageColor);
+        document.getElementById('shift2').setAttribute('style', 'background-color: ' + pageColor);
+        break;
+      case 17:
+        document.getElementById('control1').setAttribute('style', 'background-color: ' + pageColor);
+        document.getElementById('control2').setAttribute('style', 'background-color: ' + pageColor);
+        break;
+      case 18:
+        document.getElementById('option1').setAttribute('style', 'background-color: ' + pageColor);
+        document.getElementById('option2').setAttribute('style', 'background-color: ' + pageColor);
+        break;
+      default:
+        console.log(code);
+        document.getElementById(code).setAttribute('style', 'background-color: ' + pageColor);
+        break;
     }
   }
 
-  var keyRelease = function (event) {
+  var keyRelease = function(event) {
     var code = event.keyCode;
     switch (code) {
-    case 16:
-      document.getElementById('shift1').removeAttribute('style');
-      document.getElementById('shift2').removeAttribute('style');
-      break;
-    case 17:
-      document.getElementById('control1').removeAttribute('style');
-      document.getElementById('control2').removeAttribute('style');
-      break;
-    case 18:
-      document.getElementById('option1').removeAttribute('style');
-      document.getElementById('option2').removeAttribute('style');
-      break;
-    default:
-      console.log(code);
-      document.getElementById(code).removeAttribute('style');
-      break;
+      case 16:
+        document.getElementById('shift1').removeAttribute('style');
+        document.getElementById('shift2').removeAttribute('style');
+        break;
+      case 17:
+        document.getElementById('control1').removeAttribute('style');
+        document.getElementById('control2').removeAttribute('style');
+        break;
+      case 18:
+        document.getElementById('option1').removeAttribute('style');
+        document.getElementById('option2').removeAttribute('style');
+        break;
+      default:
+        console.log(code);
+        document.getElementById(code).removeAttribute('style');
+        break;
     }
   }
 
